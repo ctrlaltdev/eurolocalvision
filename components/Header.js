33b313cb88ba53b32bc7d6a0709b81120e80138a
void <yorick@ctrlalt.dev>
2019-05-10 09:02:06 -0700

    Remove copyrighted material

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,7 @@ import Link from 'next/link'
 
 const Header = () => (
   <header>
-    <h1>
-      <img alt='Eurovision Song Contest'src='https://eurovision.tv/images/logo.svg?id=0c76046d2021d3337729' />
-    </h1>
+    <h1>Not Eurovision Song Contest</h1>
     <style jsx>{`
       h1 {
         text-align: center;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
 import Home from '../layouts/Home'
 
-const Index = () => (
-  <Home>
-    <video className='Header__Vid' poster='/static/ESC2019.jpg' loop muted autoPlay>
-      <source src='/static/ESC2019.mp4' type='video/mp4' />
-    </video>
-    <style jsx>{`
-      .Header__Vid {
-        width: 100%;
-        border-bottom: 2px solid rgba(200, 230, 255, 0.75);
-        box-shadow: 0px 0px 32px 16px rgba(200, 230, 255, 1), 0px 100px 100px 40px rgba(0, 0, 0, 0.75);
-      }
-    `}</style>
-  </Home>
-)
+class Index extends React.Component {
+  constructor () {
+    super()
+  }
+
+  saveName = (e) => {
+    console.log(e.target.value)
+  }
+
+  render () {
+    return (
+      <Home>
+        <input type='text' onBlur={this.saveName} placeholder="What's your name?" />
+      </Home>
+    )
+  }
+}
 
 export default Index
